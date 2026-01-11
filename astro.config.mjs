8bc43254c88f3991837fdff76276623c051b4d41
Juan Manuel Betancur <juanbetancur142474@correo.itm.edu.co>
2026-01-11 18:35:04 -0500

    configuracion de url de github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://juan-betancurm.github.io',
+  
+  // 2. IMPORTANTE: Pon aquí el nombre de tu repositorio (con la barra al inicio)
+  base: '/juanbetancurm',
   integrations: [
     tailwind(),
     mdx()
